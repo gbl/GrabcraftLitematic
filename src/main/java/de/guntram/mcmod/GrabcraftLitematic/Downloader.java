950100cddb0473638e5e2009f12332f7bc9847fd
Guntram Blohm <gbl@guntram.de>
2020-12-27 20:29:04 +0100

    add downloading the map only, using it later (not tested)

--- src/main/java/de/guntram/mcmod/GrabcraftLitematic/Downloader.java
+++ src/main/java/de/guntram/mcmod/GrabcraftLitematic/Downloader.java
@@ -7,9 +7,14 @@ package de.guntram.mcmod.GrabcraftLitematic;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,7 +29,7 @@ class Downloader {
     
     private static String author;
     
-    public static String download(String urlString) {
+    public static String download(String urlString, boolean mapOnly) {
         author = "GrabcraftLitematica";
         if (!urlString.startsWith(urlStart)) {
             return "This seems to be wrong; needs to start with\n"+urlStart;
@@ -43,7 +48,7 @@ class Downloader {
         String[] urlParts;
         try {
             urlParts = urlString.split("/");
-            downloadResult = downloadRenderObject(urlString, urlRender+renderObject, urlParts[4]);
+            downloadResult = downloadRenderObject(urlString, urlRender+renderObject, urlParts[4], mapOnly);
         } catch (IOException ex) {
             return "Problem with schema data:\n"+ex.getMessage();
         } catch (IndexOutOfBoundsException ex) {
@@ -79,7 +84,7 @@ class Downloader {
         return null;
     }
     
-    private static String downloadRenderObject(String origURL, String urlObject, String objectName) throws IOException {
+    private static String downloadRenderObject(String origURL, String urlObject, String objectName, boolean mapOnly) throws IOException {
         RenderObject object = new RenderObject();
         InputStreamReader is = new InputStreamReader(new URL(urlObject).openStream());
         int c;
@@ -87,12 +92,36 @@ class Downloader {
         while ((c=is.read()) != -1 && c != '=')
             ;
         object.read(is);
-        BlockMap map = new BlockMap(new File("schematics", "blockmap.csv"));
-        Litematic schema = new Litematic(objectName, object.getSizeX(), object.getSizeY(), object.getSizeZ());
-        Converter.run(object, map, schema);
-        schema.setAuthor(author);
-        schema.setDescription(origURL);
-        schema.save(new File("schematics", objectName+".litematic"));
-        return "Download to "+objectName+" ok";
+        File mapFile = new File("schematics", objectName+".csv");
+        if (mapOnly || !mapFile.exists()) {
+            mapFile = new File("schematics", "blockmap.csv");
+        }
+        BlockMap map = new BlockMap(mapFile);
+        if (mapOnly) {
+            try (PrintWriter writer  = new PrintWriter(new FileWriter(mapFile))) {
+                Map<String, BlockWithStates> blockStates = map.getUsedBlocks();
+                SortedSet<String> sortedKeys = new TreeSet(blockStates.keySet());
+                for (String key: sortedKeys) {
+                    writer.append(key);
+                    BlockWithStates bws = blockStates.get(key);
+                    for (Map.Entry<String, String> state: bws.states.entrySet()) {
+                        writer.append('\t').append(state.getKey()).append('\t').append(state.getValue());
+                    }
+                    writer.append('\n');
+                }
+                if (writer.checkError()) {
+                    throw new IOException("Can't write to "+mapFile.getName());
+                }
+                writer.close();
+                return "Downloaded the map to "+mapFile.getName();
+            }
+        } else {
+            Litematic schema = new Litematic(objectName, object.getSizeX(), object.getSizeY(), object.getSizeZ());
+            Converter.run(object, map, schema);
+            schema.setAuthor(author);
+            schema.setDescription(origURL);
+            schema.save(new File("schematics", objectName+".litematic"));
+            return "Download to "+objectName+" ok";
+        }
     }
 }
